91626fbf24ca9060a7d189fc77ced8281d47b183
Cassiano <saurinho.64.ss@gmail.com>
2025-12-07 20:16:26 -0300

    Feat: disabling default security configs

---
+++ src/main/java/dev/cacassiano/workout_tracker/infra/security/SpringSecurityConfig.java
@@ -0,0 +1,20 @@
+package dev.cacassiano.workout_tracker.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+            .csrf(csfr -> csfr.disable())
+            .authorizeHttpRequests(req -> 
+                req.anyRequest().permitAll()
+            ).build();
+    }
+}
